ba2378f62d3d2ed9810925542650a64edfd48168
taqin <abdulmuttaqin456@gmail.com>
2023-05-12 13:22:22 +0700

    Update virtual_host_generator.sh

--- virtual_host_generator.sh
+++ virtual_host_generator.sh
@@ -4,7 +4,16 @@ read -p "Enter the domain name: " domain_name
 
 sudo mkdir -p /var/www/$domain_name && sudo chown -R $USER:$USER /var/www/$domain_name && sudo chmod -R 755 /var/www/$domain_name
 
-printf "<VirtualHost *:80>\n\tServerAdmin webmaster@localhost\n\tServerName $domain_name\n\tServerAlias www.$domain_name\n\tDocumentRoot /var/www/$domain_name\n\tErrorLog \${APACHE_LOG_DIR}/error.log\n\tCustomLog \${APACHE_LOG_DIR}/access.log combined\n</VirtualHost>" | sudo tee /etc/apache2/sites-available/$domain_name.conf > /dev/null
+sudo bash -c 'cat << EOF > /etc/apache2/sites-available/$domain.conf
+<VirtualHost *:80>
+    ServerAdmin admin@$domain
+    ServerName $domain_name
+    ServerAlias www.$domain_name
+    DocumentRoot /var/www/$domain_name
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOF'
 
 sudo a2ensite $domain_name.conf
 sudo apache2ctl configtest
@@ -48,8 +57,36 @@ case "$ssl_answer" in
         sudo certbot --apache -d $domain_name
 
         # Configure HTTPS virtual host
-        printf "<IfModule mod_ssl.c>\n\t<VirtualHost *:443>\n\t\tServerAdmin webmaster@localhost\n\t\tServerName $domain_name\n\t\tServerAlias www.$domain_name\n\t\tDocumentRoot /var/www/$domain_name\n\t\tErrorLog \${APACHE_LOG_DIR}/error.log\n\t\tCustomLog \${APACHE_LOG_DIR}/access.log combined\n\t\t<Directory /var/www/$domain_name/>\n\t\t\tOptions Indexes FollowSymLinks MultiViews\n\t\t\tAllowOverride All\n\t\t\tOrder allow,deny\n\t\t\tallow from all\n\t\t</Directory>\n\t\tInclude /etc/letsencrypt/options-ssl-apache.conf\n\t\tSSLCertificateFile /etc/letsencrypt/live/$domain_name/fullchain.pem\n\t\tSSLCertificateKeyFile /etc/letsencrypt/live/$domain_name/privkey.pem\n\t</VirtualHost>\n</IfModule>" | sudo tee -a /etc/apache2/sites-available/$domain_name-le-ssl.conf > /dev/null
+      sudo bash -c '<IfModule mod_ssl.c>
+<VirtualHost *:443>
+    ServerAdmin webmaster@localhost
+    ServerName $domain_name
+    ServerAlias www.$domain_name
+    DocumentRoot /var/www/$domain_name
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+  ProxyRequests Off
+    ProxyPreserveHost On
+    ProxyVia Full
 
+    <Proxy *>
+        Require all granted
+    </Proxy>
+
+    ProxyPass / http://localhost:3000/
+    ProxyPassReverse / http://localhost:3000/
+RewriteEngine on
+#RewriteCond %{SERVER_NAME} =www.app.imtaqin.id [OR]
+#RewriteCond %{SERVER_NAME} =app.imtaqin.id
+#RewriteRule ^ https://%{SERVER_NAME}%{REQUEST_URI} [END,NE,R=permanent]
+
+SSLCertificateFile /etc/letsencrypt/live/app.imtaqin.id/fullchain.pem
+SSLCertificateKeyFile /etc/letsencrypt/live/app.imtaqin.id/privkey.pem
+Include /etc/letsencrypt/options-ssl-apache.conf
+</VirtualHost>
+</IfModule>
+
+'
         sudo a2ensite $domain_name-le-ssl.conf
         sudo apache2ctl configtest
         sudo systemctl restart apache2
